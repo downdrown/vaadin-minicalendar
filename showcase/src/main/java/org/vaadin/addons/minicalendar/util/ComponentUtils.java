1677cf6f2d4cb4dbfe8ab7c245d6ef28183845d2
Manfred Huber <manfred.huber@downdrown.at>
2023-02-04 18:12:29 +0100

    Add showcase view for calendar with different DayTextStyle

--- showcase/src/main/java/org/vaadin/addons/minicalendar/util/ComponentUtils.java
+++ showcase/src/main/java/org/vaadin/addons/minicalendar/util/ComponentUtils.java
@@ -1,6 +1,8 @@
 package org.vaadin.addons.minicalendar.util;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.function.SerializableSupplier;
 
 import java.util.Map;
 
@@ -14,5 +16,19 @@ public final class ComponentUtils {
         var label = new Label(text);
         styles.forEach(label.getStyle()::set);
         return label;
+    }
+
+    public static Component header(String header) {
+        return labelWithStyles(
+            header,
+            Map.of(
+                "font-size", "var(--lumo-font-size-l)",
+                "color", "var(--lumo-primary-text-color)"
+            )
+        );
+    }
+
+    public static Component fromDefinition(SerializableSupplier<Component> definition) {
+        return definition.get();
     }
 }
