ad097dd60711ccec022842bcc7ddb77c301f6093
Manfred Huber <manfred.huber@downdrown.at>
2025-03-20 10:00:37 +0100

    chore: remove unused import

--- addon/src/main/java/org/vaadin/addons/minicalendar/MiniCalendar.java
+++ addon/src/main/java/org/vaadin/addons/minicalendar/MiniCalendar.java
@@ -38,7 +38,6 @@ import java.time.format.TextStyle;
 import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
